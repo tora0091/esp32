ba32b4a9ba7ed0c0ca9e49d16694d23d59ccf789
Shiro Aiba <tora0091@yahoo.co.jp>
2018-10-01 20:55:55 +0900

    digitalRead and digitalWrite sample program

---
+++ sketch_blink_button/sketch_blink_button.ino
@@ -0,0 +1,20 @@
+const int buttonPin = 4;
+const int ledPin = 16;
+
+int buttonState = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
