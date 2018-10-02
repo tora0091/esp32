83b0f13da526b1e0296635e3c55839722478eaae
Shiro Aiba <tora0091@yahoo.co.jp>
2018-10-02 13:17:27 +0900

    add unit4

---
+++ sketch_unit4_1/sketch_unit4_1.ino
@@ -0,0 +1,14 @@
+const int potPin = 34;
+
+int potValue = 0;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+}
+
+void loop() {
+  potValue = analogRead(potPin);
+  Serial.println(potValue);
+  delay(500);
+}
