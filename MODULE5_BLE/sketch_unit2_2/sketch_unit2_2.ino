99ed6f2a261a6db1a08cb73568a01bd20c489815
Shiro Aiba <tora0091@yahoo.co.jp>
2018-10-08 08:31:01 +0900

    add sketch

---
+++ MODULE5_BLE/sketch_unit2_2/sketch_unit2_2.ino
@@ -0,0 +1,31 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+
+int scanTime = 30;
+
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+  }
+};
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Scanning...");
+
+  BLEDevice::init("");
+  BLEScan* pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(true);
+
+  BLEScanResults foundDevices = pBLEScan->start(scanTime);
+  Serial.print("Devices found: ");
+  Serial.println(foundDevices.getCount());
+  Serial.println("Scan done!");
+}
+
+void loop() {
+  delay(2000);
+}
